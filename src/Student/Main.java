cff7f71eec8a8bcbca42d0ccd9151158f64ce25f
kkarbowy <k.karbowy2401@gmail.com>
2024-05-04 20:49:19 +0200

    Added getters and setters and simplified Main class.

--- src/Student/Main.java
+++ src/Student/Main.java
@@ -2,43 +2,48 @@ package Student;
 
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student(1, "Karolina", "Kolorowa", 132142152);
-        System.out.println(student1.id);
-        System.out.println(student1.name);
-        System.out.println(student1.surname);
-        System.out.println(student1.phoneNumber);
+        //stworzenie 1 studenta i wyświetlenie kilku parametrów
+        Student student1 = new Student(1, "Karolina", "Nowak", 132142152);
+        System.out.println(student1.getId());
+        System.out.println(student1.getName());
+        System.out.println(student1.getSurname());
+        System.out.println(student1.getPhoneNumber());
 
-        Student student2 = new Student(1, "Karolina", "Kolorowa", "madryczlowiek@gmail.com");
-        System.out.println(student2.id);
-        System.out.println(student2.name);
-        System.out.println(student2.surname);
-        System.out.println(student2.email);
+        //stworzenie 2 studenta i wyświetlenie kilku parametrów
+        Student student2 = new Student(2, "Laura", "Kowalska", "testmail1@gmail.com");
+        System.out.println(student2.getId());
+        System.out.println(student2.getName());
+        System.out.println(student2.getSurname());
+        System.out.println(student2.getEmail());
 
-        System.out.println(student1.czyZdane);
+        //ustawienie parametru czyZdane na "true"
+        System.out.println(student1.isCzyZdane());
         student1.przepuscStudenta();
-        System.out.println(student1.czyZdane);
+        System.out.println(student1.isCzyZdane());
 
-        student1.zmienEmail("madrakobietka@gmail.com");
-        System.out.println(student1.email);
+        //zmiana maila
+        student1.zmienEmail("testmail2@gmail.com");
+        System.out.println(student1.getEmail());
 
-        student1.dodajPrzedmiot("matematyka");
-        student1.dodajPrzedmiot("polski");
-        System.out.println(student1.przedmioty);
-        System.out.println(student2.przedmioty);
+        //dodanie przedmiotów do studenta 1
+        //tablica przedmiotów
+        String[] przedmiotyDoWpisania = {"matematyka", "polski", "fizyka", "geografia"};
+        for (String wpisanyPrzedmiot : przedmiotyDoWpisania) {
+            student1.dodajPrzedmiot(wpisanyPrzedmiot);
+        }
+        System.out.println(student1.getPrzedmioty());
 
-        student1.dodajOcene(5.0f);
-        System.out.println(student1.oceny);
+        //dodanie ocen do studenta 1
+        //tablica ocen
+        Float[] ocenyDoWpisania = {5.0f, 4.0f, 5.0f, 3.0f, 4.5f, 5.0f, 5.0f};
+        for (float wpisanaOcena : ocenyDoWpisania) {
+            student1.dodajOcene(wpisanaOcena);
+        }
+        System.out.println(student1.getOceny());
 
-        student1.dodajOcene(4.0f);
-        student1.dodajOcene(5.0f);
-        student1.dodajOcene(3.0f);
-        student1.dodajOcene(4.5f);
-        student1.dodajOcene(5.0f);
-        student1.dodajOcene(5.0f);
-
+        //sprawdzenie czy student 1 otrzyma stypendium
         float sredniaStudenta1 = student1.wyliczSredniaOcen();
         System.out.println("Srednia ocen Studenta1 " + sredniaStudenta1);
         student1.czyDostanieStypendium();
-
     }
 }
